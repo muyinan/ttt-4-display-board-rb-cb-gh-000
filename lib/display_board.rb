d6c01cea0f776cd53b82ec18938ea51e556300a0
Dorna Mu <dorna.mu@gmail.com>
2018-07-10 00:29:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 def display_board(a)
-  puts "#{a[0]}   | #{a[1]}  |#{a[2]}   "
+  puts " #{a[0]} | #{a[1]} | #{a[2]} "
   puts "-----------"
-  puts " #{a[3]}  |#{a[4]}   |#{a[5]}   "
+  puts " #{a[3]} | #{a[4]} | #{a[5]} "
   puts "-----------"
-  puts "#{a[6]}   | #{a[7]}  | #{a[8]}  "
+  puts " #{a[6]} | #{a[7]}| #{a[8]} "
 end
